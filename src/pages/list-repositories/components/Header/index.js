38881c5b9bba56d573801086eefad6647d121ab5
Fábio <fsmaiorano@gmail.com>
2018-02-24 16:00:59 -0300

    add: Header Component

---
+++ src/pages/list-repositories/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Header = () => (
+  <View>
+    <Text>Header</Text>
+  </View>
+);
+
+export default Header;
--- src/pages/list-repositories/index.js
+++ src/pages/list-repositories/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Header from './components/Header';
+
 const ListRepositories = () => (
   <View>
+    <Header />
     <Text>List Repositories</Text>
   </View>
 );
