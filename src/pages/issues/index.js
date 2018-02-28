b41242657efa8387be91f4c8aeea7cb459a0b0a2
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-02-28 07:20:09 -0300

    add: AsyncStorage for filters

--- src/pages/issues/index.js
+++ src/pages/issues/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, ActivityIndicator, FlatList, TouchableOpacity, AsyncStorage } from 'react-native';
 import PropTypes from 'prop-types';
 import api from 'services/api';
 import styles from './styles';
@@ -33,18 +33,32 @@ class Issues extends Component {
 
   state = {
     issues: [],
+    filter: '',
     loading: true,
   };
 
   componentDidMount = async () => {
     this.getIssues();
+    this.getFilter();
   };
 
   getIssues = async () => {
     const { repository } = this.props.navigation.state.params;
     const issues = await api.get(`/repos/${repository.owner.login}/${repository.name}/issues`);
-    console.tron.log(issues);
     this.setState({ issues: issues.data, loading: false });
+  }
+
+  setFilter = async (filter) => {
+    await AsyncStorage.setItem('@Github_Issues_Watcher:filter', filter);
+    this.setState({ filter });
+  }
+
+  getFilter = async () => {
+    const filter = await AsyncStorage.getItem('@Github_Issues_Watcher:filter');
+    console.tron.log(filter);
+    if (filter) {
+      this.setState({ filter });
+    }
   }
 
   renderListItem = ({ item }) => (
@@ -59,15 +73,15 @@ class Issues extends Component {
 
   renderFilters = () => (
     <View style={styles.filters}>
-      <TouchableOpacity style={styles.filter}>
+      <TouchableOpacity style={styles.filter} onPress={() => this.setFilter('Todas')}>
         <Text style={styles.filterText}>{'Todas'} </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.filter}>
+      <TouchableOpacity style={styles.filter} onPress={() => this.setFilter('Abertas')}>
         <Text style={styles.filterText}>{'Abertas'} </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.filter}>
+      <TouchableOpacity style={styles.filter} onPress={() => this.setFilter('Fechadas')}>
         <Text style={styles.filterText}>{'Fechadas'} </Text>
       </TouchableOpacity>
     </View>
